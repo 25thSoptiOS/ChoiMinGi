3c7e64ac0fe0f359f5954988103a46971e3589b2
minkichoe <minki.developer@gmail.com>
2019-10-01 17:49:55 +0900

    feat: 세 번째 실습
    IBOutlet 생성
    IBAction 생성
    
    인사하기 버튼 누를 시 라벨이 인사말로 변경

--- week1/helloworld/helloworld/Sources/ViewControllers/ViewController.swift
+++ week1/helloworld/helloworld/Sources/ViewControllers/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onGreet(_ sender: Any) {
+        titleLabel.text = "Hi, SOPT!"
+    }
 }
 
