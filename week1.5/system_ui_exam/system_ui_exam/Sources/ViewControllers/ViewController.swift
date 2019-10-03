4112f98bfc08a49c2d3fdcda9280b4f04622f8a9
minkichoe <minki.developer@gmail.com>
2019-10-03 19:00:42 +0900

    feat: 세 번째 실습
    세 번째 실습
    
    UISwitch를 사용하여 Image View의 이미지와 Label.text를 변경

--- week1.5/system_ui_exam/system_ui_exam/Sources/ViewControllers/ViewController.swift
+++ week1.5/system_ui_exam/system_ui_exam/Sources/ViewControllers/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lightLabel: UILabel!
+    @IBOutlet weak var lightImage: UIImageView!
+    @IBOutlet weak var lightSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onSwitchClicked(_ sender: Any) {
+        let imgSrc: String!
+        let labelText: String!
+        
+        if (lightSwitch.isOn) {
+            imgSrc = "candle-on"
+            labelText = "Candle is On"
+        } else {
+            imgSrc = "candle-off"
+            labelText = "Candle is Off"
+        }
+        
+        if let image = UIImage(named: imgSrc) {
+            lightImage.image = image
+            lightLabel.text = labelText
+        }
+    }
 }
 
